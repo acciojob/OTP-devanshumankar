c0f8009301c7389cd2e4a308b208949f39dcae81
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-12-01 16:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const container = document.querySelector(".code-container")
+        container.addEventListener("input", (e) => {
+            const ele = e.target
+            if (ele.value.length == 1) {
+                ele.nextElementSibling.focus()
+            }
+
+        })
+        const inputs = container.querySelectorAll(".code");
+
+        inputs.forEach((input, index) => {
+            input.addEventListener("keydown", (e) => {
+                if (e.key === "Backspace" && input.value.length === 0) {
+                    if (index > 0) {
+                        inputs[index - 1].focus();
+                    }
+                }
+            });
+        });
